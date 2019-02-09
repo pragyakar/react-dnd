ff7d164ced842206919c4e9d60103c4d4dbbaaa3
pragyakar <pragyakar75@gmail.com>
2019-02-09 20:15:40 +0545

    :sparkles: Added component structure

---
+++ src/components/draggable/draggable.jsx
@@ -0,0 +1,9 @@
+import * as  React from 'react';
+
+class Draggable extends React.Component {
+  render() {
+    return 'Draggable Component'
+  }
+}
+
+export default Draggable;
---
+++ src/components/droppable/droppable.jsx
@@ -0,0 +1,9 @@
+import * as  React from 'react';
+
+class Droppable extends React.Component {
+  render() {
+    return 'Droppable Component'
+  }
+}
+
+export default Droppable;
