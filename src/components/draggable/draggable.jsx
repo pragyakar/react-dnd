e12406593ea2ca126b8f9760ac9ee3d64dfdb430
pragyakar <pragyakar75@gmail.com>
2019-02-09 20:26:40 +0545

    :sparkles: Added event listener functions

--- src/components/draggable/draggable.jsx
+++ src/components/draggable/draggable.jsx
@@ -1,8 +1,22 @@
 import * as  React from 'react';
 
 class Draggable extends React.Component {
-  render() {
-    return 'Draggable Component'
+
+  drag = (e) => {
+    e.dataTransfer.setData('transfer', e.target.id);
+
+  }
+
+  dontAllowDrop = (e) => {
+    e.stopPropagation();
+  }
+
+   render() {
+    return (
+      <div id={this.props.id} draggable='true' onDragStart={this.drag} onDragOver={this.dontAllowDrop}>
+        {this.props.children}
+      </div>
+    )
   }
 }
 
--- src/components/droppable/droppable.jsx
+++ src/components/droppable/droppable.jsx
@@ -1,8 +1,23 @@
 import * as  React from 'react';
 
 class Droppable extends React.Component {
+
+  drop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('transfer');
+    e.target.appendChild(document.getElementById(data));
+  }
+
+  allowDrop = (e) => {
+    e.preventDefault();
+  }
+
   render() {
-    return 'Droppable Component'
+    return (
+      <div id={this.props.id} onDrop={this.drop} onDragOver={this.allowDrop}>
+        {this.props.children}
+      </div>
+    )
   }
 }
 
